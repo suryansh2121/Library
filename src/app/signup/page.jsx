c5817e7e672851a1cd60e7e2e0f59f96a76ccf43
suryansh2121 <rajputsuryansh491@gmail.com>
2025-04-19 07:10:34 +0530

    signup page added

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>signup here
+        <div className="google"> signup with google</div>
+        <div className="google"> signup with github</div>
+        <div className="google"> signup with facebook</div>
+    </div>
+  )
+}
+
+export default page
